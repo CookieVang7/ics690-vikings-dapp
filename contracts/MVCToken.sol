04593d3777d45499bc4773c4f6e70d60000826db
CookieVang7 <cookie.vang6700@gmail.com>
2024-04-02 01:34:37 -0500

    Add MVC mint function

--- contracts/MVCToken.sol
+++ contracts/MVCToken.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.19;
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract MVCToken is ERC20 {
-    constructor (string memory name_, string memory symbol_) ERC20(name_,symbol_) public {
+    constructor (string memory name_, string memory symbol_) ERC20(name_,symbol_) public {}
+
+    function mint(address account, uint256 amount) public {
+        _mint(account, amount);
     }
 }
--- contracts/SkolFaithful.sol
+++ contracts/SkolFaithful.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.19;
 
 import "./safemath.sol";
+import "./MVCToken.sol" as MVCTokenContract;
+import "./REPToken.sol" as REPTokenContract;
 
 // to interact with a deployed instance of this contract after running command "truffle console":
 // SkolFaithful.deployed().then(function(instance) {app = instance})
@@ -10,6 +12,7 @@ import "./safemath.sol";
 
 contract SkolFaithful {
     using SafeMath for uint;
+    MVCTokenContract.MVCToken public mvcToken;
 
     struct Member {
         address owner;
@@ -24,7 +27,7 @@ contract SkolFaithful {
     uint public skolNationCount;
     Member public clientUser;
 
-    constructor () {
+    constructor (MVCTokenContract.MVCToken myMvcToken) {
         address temp1 = 0xf220d553fbbC28b6f381CbB2bE99D59De42d2F84;
         address temp2 = 0xFeb798ed0E1eC865Bf80703cA1E1Bb7a48DdEAfa;
         address temp3 = 0x48f84a00F895be17BD4Fa9e0731c7b39eAcd6FBe;
@@ -35,11 +38,29 @@ contract SkolFaithful {
         addMember(temp3); // Brandon Staley
         addMember(temp4); // Frank Reich
         addMember(temp5);
+
+        mvcToken = myMvcToken;
     }
 
     // Adding members to the skolNation map giving them initial values of 4 MVCs and 0 REP tokens
-    function addMember(address _owner) private {
+    function addMember(address owner) private {
         skolNationCount = skolNationCount.add(1);
-        skolNation[_owner] = Member(_owner,4,0);
+        skolNation[owner] = Member(owner,0,0);
+
+        mvcToken.mint(owner,4);
     }
+
+    event newDAOMember(address newUser);
+
+    // function for a new user to join the DAO
+    function joinDAO(address owner) public payable {
+        uint ethAmount = 2;
+        require(msg.value == ethAmount.toWei(), 'Insufficient funds provided');
+
+        addMember(owner);
+
+        emit newDAOMember(owner);
+    }
+
+    // TODO: condense SkolFaithful and CoachVotingMechanism into a voting contract with the redistribution mechanism
 }
